d7237f28ede8fb6de59c784b0ab76fc61decd9f1
eanp <awifamz@gmail.com>
2023-03-08 15:49:18 +0700

    crud

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,78 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import NavbarMenu from '../../Component/NavbarMenu'
+import axios from 'axios'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+let url = 'https://zealous-erin-cod.cyclic.app/recipes'
 
 export default function Home() {
+  const [data,setData] = useState()
+  const [show, setShow] = useState(false);
+  const [selected,setSelected] = useState()
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const confirmDelete = (id) => {
+    setSelected(id)
+    handleShow()
+  }
+
+  
+  useEffect(()=>{
+    getData()
+  },[])
+  
+  const getData = () => {
+    axios.get(url).then((res)=>{
+      console.log(res)
+      setData(res.data.data)
+    }).then((err)=>{
+      console.log(err)
+    })
+  }
+
+
+  const deleteData = (id) => {
+    axios.delete(url+`/${id}`).then((res)=>{
+      console.log("delete data index ke ", id)
+      console.log(res)
+      handleClose()
+      getData()
+    }).then((err)=>{
+      console.log(err)
+    })
+  }
+
   return (
     <div>
         <NavbarMenu />
         <h1>Home</h1>
-        <button className='btn btn-primary py-5' style={{paddingLeft:'20px',paddingRight:'20px',backgroundColor:'skyblue'}}>detail</button>
+        <div className="container">
+          {data?.map((item,index)=>(
+            <div key={index+1}>
+              <h5>{item.title}</h5>
+              <img className='img-thumbnail' src={item.photo} style={{height:"100px"}} />
+            <button className='btn btn-danger' onClick={()=>confirmDelete(index)}>
+              delete menu
+            </button>
+            </div>
+          ))}
+        </div>
+
+      <Modal show={show} onHide={()=>handleClose()}>
+        <Modal.Header closeButton>
+          <Modal.Title>Kamu yakin hapus data ini</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={()=>handleClose()}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={()=>deleteData(selected)}>
+            Delete data
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
